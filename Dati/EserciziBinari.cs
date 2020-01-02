b18f2f31d4b199b82a3cc23957915f9cf8741322
Riccardo Rossi <riccardorossi.r2004@outlook.it>
2020-01-02 10:46:55 +0100

    Lavoro di Natale

--- Dati/EserciziBinari.cs
+++ Dati/EserciziBinari.cs
@@ -20,16 +20,17 @@ namespace Dati
             int c = -b;//Valore assegnato dovrà essere l' opposto alla variabile b;
             Console.WriteLine(Convert.ToString(b, 2));//Valore b converito in binario;
             Console.WriteLine(Convert.ToString(c, 2));//valore c convertito in binario;
-            int value = 31;
+            Console.WriteLine("Inserisci un numero intero: ");
+            int value = int.Parse(Console.ReadLine());
             int somma = value;
             value = ~value + 1; //NOT
             //value=value+1;
             somma = somma + value;
-            Console.WriteLine($"{value} e {somma}");
+            Console.WriteLine($"l'opposto del numero inserito{value} e la somma tra value + somma(tra numero inserito e quello opposto){somma}");
             long number = 55555;
-            Console.WriteLine(Convert.ToString(number, 2));
-            short number1 = (short)number;
-            Console.WriteLine(number1);
+            Console.WriteLine(Convert.ToString(number, 2));//Il numero(number) viene convertito in binario;
+            short number1 = (short)number;//poi da tipo long viene trasformato a tipo short;
+            Console.WriteLine(number1);//viene letto il numero;
             Console.ReadLine();
 
         }
